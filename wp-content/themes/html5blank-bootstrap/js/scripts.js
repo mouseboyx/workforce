ab0a406a3290e4cf7e6bb80d13ecf69dd93354a9
mouseboyx <mouseboyx@gmail.com>
2018-02-08 17:51:35 -0600

    HTML5blank theme added

---
+++ wp-content/themes/html5blank-bootstrap/js/scripts.js
@@ -0,0 +1,20 @@
+(function ($, root, undefined) {
+	
+	$(function () {
+		
+		'use strict';
+		// DOM ready, take it away
+		
+	});
+	
+})(jQuery, this);
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
