25be6cd64f3aa8cb67c8a34237259ce013d3d57c
1MLY <1000000lightyears@gmail.com>
2018-03-28 19:56:26 -0500

    fixed responsive nav issue

--- wp-content/themes/html5blank-stable/header.php
+++ wp-content/themes/html5blank-stable/header.php
@@ -8,7 +8,7 @@
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
         <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet">
-        <link href="<?php echo get_template_directory_uri(); ?>/css/mdb.min.css" rel="stylesheet">
+        <!-- <link href="<?php echo get_template_directory_uri(); ?>/css/mdb.min.css" rel="stylesheet"> -->
         <link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -33,11 +33,15 @@
 			<!-- nav -->
 
 			<nav class="navbar navbar-expand-md navbar-light">
-				<form class="form-inline my-2 my-lg-0" id="searchbar">
-				      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-				      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-				    </form>
-				<div class="container-fluid nowrap">
+				<div class="input-group col-md-4" id="searchbar">
+            <input class="form-control py-2 border-right-0 border" type="search" value="search" id="example-search-input">
+            <span class="input-group-append">
+                <button class="btn btn-outline-secondary border-left-0 border" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+				<div class="container-fluid">
 	      			<a class="navbar-brand" href="<?php echo home_url(); ?>/home">
 	      				<div class="header-logos">
 	      					<img class="img-fluid" id="workforce-logo" src="http://workforce.test/wp-content/uploads/2018/03/workforce-logo-2.png" alt="Workforce Partnership">
@@ -48,8 +52,23 @@
 	        			<span class="navbar-toggler-icon"></span>
 	      			</button>
 
-		      		<div class="collapse navbar-collapse align-end" id="navbarCollapse">
-
+		      		<div class="collapse navbar-collapse align-end text-center" id="navbarCollapse">
+		      			<!-- <form class="form-inline">
+						  	<input class="form-control mr-sm-2 py-2 border-right-0 border" type="search" placeholder="Search" aria-label="Search">
+						    <span class="input-group-append">
+				                <button class="btn btn-outline-secondary border-left-0 border" type="button">
+				                    <i class="fa fa-search"></i>
+				                </button>
+				            </span>
+						</form> -->
+		      			<div class="input-group col-md-4" id="searchbar2">
+				            <input class="form-control py-2 border-right-0 border" type="search" placeholder="search" id="example-search-input">
+				            <span class="input-group-append">
+				                <button class="btn btn-outline-secondary border-left-0 border" type="button">
+				                    <i class="fa fa-search"></i>
+				                </button>
+				            </span>
+				        </div>
 		      			<?php
 					        wp_nav_menu( array(
 					            'theme_location'    => 'primary',
@@ -62,6 +81,7 @@
 					            'walker'            => new WP_Bootstrap_Navwalker()
 							) );
 				        ?>
+
 		      		</div>
 		      </div>
     		</nav>
